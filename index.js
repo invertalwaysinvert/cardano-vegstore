53d2d467df66f8893594b9b6f710ca7028aef8da
cheffNFT <cheffwallets@gmail.com>
2021-07-22 00:07:10 +0100

    set port to 80

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const axios = require('axios');
  */
 
 const app = express();
-const port = process.env.PORT || "8000";
+const port = process.env.PORT || "80";
 
 var corsOptions = {
   origin: process.env.FRONT_END,
